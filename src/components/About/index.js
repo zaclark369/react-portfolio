449c688523a4c013c6860d596f95d060f335abc6
Zachary Clark <zacclark369@gmail.com>
2022-04-03 17:57:09 -0700

    updated About to be more modular

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,37 +1,16 @@
 import React from "react";
 
-
 function About(props) {
-return(
-<section className="hero">
-<div className="hero-container hero-picture">
-  <img src="./assets/images/profile.jpg" alt="Profile Picture" />
-</div>
-<div className="hero-container">
-  <a id="AboutMe"></a>
-  <h2>About Me</h2>
-  <p className="paragraphdesc">
-    I am an aspiring web developer who loves to work on exciting and
-    challenging projects. I have worked in several fields, with my current focus on contract processing and system
-    improvement. 
-  </p>
-</div>
-<div className="herotext">
-  <div className="herotexttop">
-    <h1>I’m ready to design your website and build it</h1>
-    <p className="paragraphdesc">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-      Temporibus quasi obcaecati rerum magnam qui soluta, minus
-      recusandae eaque culpa autem tempore saepe dolorum vero reiciendis
-      illum nobis magni ratione blanditiis.
-      <br />
-    </p>
-  </div> 
-   <a href="#capabilities" className="buttondown">Check out my capabilities</a> 
-</div>
-</section>
-
-)
+  return (
+    <div className="about">
+      <h2>About Me</h2>
+      <p className="paragraphdesc">
+        I am an aspiring web developer who loves to work on exciting and
+        challenging projects. I have worked in several fields, with my current
+        focus on contract processing and system improvement.
+      </p>
+    </div>
+  );
 }
 
 export default About;
