d9455f5f43594edb28c0541d409febc2d50f8335
Zachary Clark <zacclark369@gmail.com>
2022-04-03 18:23:13 -0700

    trying to do to complicated of a dynamic portfolio. Will go back to normal, then try again later in another branch. Imported contact sheet and helpers from module!

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,136 +1,77 @@
 import React, { useState, useEffect } from "react";
 
 function Portfolio() {
-  const projectList = [
-    "weather-plotter",
-    "Pantry",
-    "gist",
-    "employee-tracker",
-    "dinner&amovie",
-  ];
-  const [project, setProject] = useState([]);
-  const [hover, setHover] = useState("");
+  // const projectList = [
+  //   "weather-plotter",
+  //   "Pantry",
+  //   "gist",
+  //   "employee-tracker",
+  //   "dinner&amovie",
+  // ];
+  // const [project, setProject] = useState([]);
+  // const [hover, setHover] = useState("");
 
-  async function getPortfolio() {
-    const portfolio = [];
-    let i = 1;
-    for (const project of projectList) {
-      const response = await fetch(
-        `https://api.github.comrepos/zaclark369/${project}`
-      );
-      const data = await response.json();
-      await portfolio.push({
-        name: data.name,
-        description: data.description,
-        gitLink: data.svn_url,
-        liveLink: data.homepage,
-        index: index,
-        topics: data.topics,
-      });
-      i++
-    };
-    setProject(portfolio);
-  };
+  // async function getPortfolio() {
+  //   const portfolio = [];
+  //   let i = 1;
+  //   for (const project of projectList) {
+  //     const response = await fetch(
+  //       `https://api.github.comrepos/zaclark369/${project}`
+  //     );
+  //     const data = await response.json();
+  //     await portfolio.push({
+  //       name: data.name,
+  //       description: data.description,
+  //       gitLink: data.svn_url,
+  //       liveLink: data.homepage,
+  //       index: index,
+  //       topics: data.topics,
+  //     });
+  //     i++
+  //   };
+  //   setProject(portfolio);
+  // };
+
+  // function handleClick(e) {
+  //   const cards = e.target;
+  //   cards.scroll({ block: 'center'});
+  //   (cards.className.includes("selected-card")) ? setHover('') : setHover(cards.id)
+  // }
 
   return (
-    <div></div>
-    // <div className="projects" id="projects">
-    //         <div className='section-title'>
-    //           <h2>Projects</h2>
-    //         </div>
-    //           <section className="section-content">
+    <div>
+      {/* <p className="loading">Loading...</p>
 
-    //             <article className="boxmodel">
-    //               <a href="https://zaclark369.github.io/run-buddy/" target="_blank">
-    //                 <img
-    //                   src="./assets/images/run-buddy-screengrab.png"
-    //                   alt="Completed run buddy website"
-    //                 />
+      {project.map((Data) => (
+        <div key ={data.i} id={data.name} className={`pCard ${selected === data.name && "selected-card"}`} >
+          <h4>{data.name}</h4>
+          <div className="img-container">
+            <a href= {data.gitlink} target="_blank">
+              <img className="github" src={require('../../assets/images/github.png')} alt="github" />
+            </a>
+      </div>
 
-    //               </a>
-    //               <div className="boxtext">
-    //                 <h3>Run Buddy Landing site</h3>
-    //               <p>
-    //                 Built a landing page and privacy policy to match the given mock up for a training company.
-    //                 <br/>
-    //                 Technologies used: HTML, CSS, GIT
-    //               </p>
-    //             </div>
-    //             </article>
-    //             <article className="boxmodel">
-    //               <a
-    //                 href="https://zaclark369.github.io/Horiseon-Accessability/"
-    //                 target="_blank"
-    //               >
-    //                 <img
-    //                   src="./assets/images/horiseon-screen-capture.png"
-    //                   alt="Completed Horiseon Accessability website"
-    //                 />
-    //               </a>
-    //               <div className="boxtext">
-    //                 <h3>Horeiseon Accessability Landing Page</h3>
-    //               <p>
-    //                 HTML based page that we optomized for SEO and accessibility.
-    //                 <br/>
-    //                 Technologies used: HTML, CSS, GIT
-    //               </p>
-    //             </div>
-    //             </article>
-    //             <article className="boxmodel">
-    //                 <a href="https://zaclark369.github.io/robot-gladiators/" target="_blank">
-    //                     <img src="./assets/images/robot-gladiator_Screenshot.png" alt="Chrome window showing prompt box that runs game."/>
-    //                 </a>
-    //                 <div className="boxtext">
-    //                     <h3>Robot Gladiators</h3>
-    //                 <p>
-    //                     Simple JavaScript game
-    //                     <br/>
-    //                     Technologies used: HTML, JavaScript, GIT
-    //                 </p>
-    //             </div>
-    //             </article>
-    //             <article className="boxmodel">
-    //                 <a href="https://zaclark369.github.io/Password-Generator/" target="_blank">
-    //                     <img src="./assets/images/password-generator-screengrab.png" alt="Screenshot of password generator home page"/>
-    //                 </a>
-    //                 <div className="boxtext">
-    //                     <h3>Password Generator</h3>
-    //                 <p>
-    //                     Password generator using prompts to user for upper case, lower case, and special characters.
-    //                     <br/>
-    //                     Technologies used: HTML, JavaScript, CSS, GIT
-    //                 </p>
-    //             </div>
-    //             </article>
-    //             <article className="boxmodel">
-    //               <a href="https://christopherlawn.github.io/team-1-dinner-movie/" target="_blank">
-    //                   <img src="./assets/images/chew-and-view-screengrab.png" alt="Screenshot of chew and view home page" />
-    //               </a>
-    //               <div className="boxtext">
-    //                   <h3>Chew and View</h3>
-    //               <p>
-    //                   Front end website for linking a zipcode based restaurant and movie together for a potential date night.
-    //                   <br/>
-    //                   Technologies used: HTML, JavaScript, CSS, GIT, Materialize Framework
-    //               </p>
-    //           </div>
-    //           </article>
-    //           <article className="boxmodel">
-    //             <a href="https://whispering-reef-71968.herokuapp.com/" target="_blank">
-    //                 <img src="./assets/images/screenshotHomepage.png" alt="Screenshot of pantry home page"/>
-    //             </a>
-    //             <div className="boxtext">
-    //                 <h3>Pantry</h3>
-    //             <p>
-    //               A full stack application utilizing sql practices of Get/Post to find recipes from a database. They can be viewed or even saved with an account.
-    //               <p>Blurb: Have you ever wanted to make dinner but can't think of anything. This application will take what you have in your pantry and suggest appropriate recipes using the ingredients you already have. Create an account and you can save your favorites for future viewing. Keyword buttons will help you quickly reach an easy meal to make and visual recipe cards will show you how good home cooking can look.</p>
-    //                 <br/>
-    //                 Technologies used: HTML, JavaScript, CSS, GIT, Bootstrap Framework, Sequalize, MySQL, npm, NodeJS
-    //             </p>
-    //         </div>
-    //         </article>
-    //         </section>
-    //         </div>
+      <img className="screenshot" style={{ "display": selected === data.name ? "block" : "none" }} src={require(`../../assets/images/${data.name}.jpg`)} alt={`Screenshot of ${data.name}`}/>
+
+      <p>{data.description}</p>
+                    <p style={{ "display": selected === data.name ? "none" : "block", "margin": "0","fontSize": "16px"}}>Click for more...</p>
+        
+                    <p  className="tech-title" style={{ "display": selected === data.name ? "block" : "none" }}>  
+                    Technologies:
+                    <ul className="columns-list">
+                        {data.topics.map((topic) => (
+                                <li key={topic}> {topic} </li>
+                        ))}
+                        </ul>
+                    </p> 
+                
+                    <div className={`liveLinkDiv  ${selected === data.name && "reset"} `} >
+                        {data.liveLink && <a className='liveLink' href={data.liveLink} target='__blank'>Live Link</a>}
+                    </div>
+                </div>
+            ))} */}
+        </div>
+  
   );
 }
 
