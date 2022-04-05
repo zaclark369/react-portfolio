3d73e1c23c1590ad4b73f4048d676de6b98b9ca9
Zachary Clark <zacclark369@gmail.com>
2022-04-04 22:07:12 -0700

    finally got nav working, turned out I had  ! instead of an =

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
-import React, {useState} from 'react';
-import "./index.css"
+import React, { useState } from "react";
+import "./index.css";
 
-import './App.css';
-import Nav from './components/Nav';
-import About from './components/About';
-import Contact from './components/Contact';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
+import "./App.css";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
 
 function App() {
+  const [view, setView] = useState("about");
 
- const [view, setView] = useState("about")
-  
   // const handleView = (page) => {
   //   setView(page);
   //   }
-  
 
   return (
     <div className="App">
+      <div>
       <Header></Header>
-        <Nav view={view} setView={setView}></Nav>
-      
+      <Nav view={view} setView={setView}></Nav>
+      </div>
       <main>
-      {view !== "contact" ? (
-        <>
-        {view !== "about" && <About/>}
-        {view !== "portfolio" && <Portfolio/>}
-        {view !== "resume" && <Resume/>}
-        
-        </>
-      ) : (
-        <Contact />
-      )}
+        {view !== "contact" ? (
+          <>
+            {view === "portfolio" && <Portfolio />}
+            {view === "resume" && <Resume />}
+            {view === "about" && <About />}
+          </>
+        ) : (
+          <Contact className="form-inline" />
+        )}
       </main>
-      <Footer className="d-flex"></Footer>
-   </div>
-   
+      <div>
+      <Footer/>
+      </div>
+    </div>
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,41 +1,55 @@
-import React from 'react';
-
+import React from "react";
 
 function Nav(props) {
+  const { view, setView } = props;
 
-  const {view, setView} = props;
-
-    return (
-        
-       
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <li className = "navbar-brand"></li>
-                    <div className="navbar-collapse collapse" id='navbarSupportedContent'>
-                      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                      {/* <li className="nav-link" onClick={() => props.handleView('default')}>Home
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <li className="navbar-brand"></li>
+        <div className="navbar-collapse collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {/* <li className="nav-link" onClick={() => props.handleView('default')}>Home
                             </li> */}
-                            <li className="nav-link" >
-                              <a href="#About" className="btn" onClick={() => setView('about')}>About</a>
-                            </li>
-                            
-                            <li className="nav-link" >
-                              <a href="#Portfolio" className="btn" onClick={() => setView('portfolio')}>Portfolio</a>
-                            </li>
-                            <li className="nav-link" >
-                              <a href="#Resume" className="btn" onClick={() => setView('resume')}>Resume</a>
-                            </li>
-                            <li className="nav-link">
-                              <a href="#Contact" className="btn" onClick={() => setView('contact')}>Contact</a>
-                            </li>
-                            
-                      </ul>
-                    </div>
 
-                </div>
-                </nav>
-       
-    )
+            <li className="nav-link">
+              <a href="#About" className="btn" onClick={() => setView("about")}>
+                About
+              </a>
+            </li>
+            <li className="nav-link">
+              <a
+                href="#Portfolio"
+                className="btn"
+                onClick={() => setView("portfolio")}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-link">
+              <a
+                href="#Resume"
+                className="btn"
+                onClick={() => setView("resume")}
+              >
+                Resume
+              </a>
+            </li>
+
+            <li className="nav-link">
+              <a
+                href="#Contact"
+                className="btn"
+                onClick={() => setView("contact")}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
