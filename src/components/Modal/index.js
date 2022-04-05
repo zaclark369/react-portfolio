408feaeaf92b41fe7c3a3e73f6dd4ff04218a987
Zachary Clark <zacclark369@gmail.com>
2022-04-04 22:31:50 -0700

    working on finishing up repo portfolio

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 
 const Modal = ({ onClose, currentProject }) => {
-  const { name, description, category } = currentProject;
+//   const { name, description, category } = currentProject;
 
   return (
     <div className="modalBackdrop">
       <div className="modalContainer">
-        <h3 className="modalTitle">{name} </h3>
-        <img
-          src={require(`../../assets/projects/${category}/.jpg`).default}
-          alt="current category"
-        />
-        <p>{description}</p>
+      <>
+      {projects.map((project) => (
+        <div className="py-3" key={`portfolio${project.key}`}>
+          <h2 key={`h${project.key}`}>
+            <a
+              href={project.live}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="text-decoration-none project-header"
+            >
+              {project.name}
+            </a>
+          </h2>
+          <div key={`div${project.key}`} className="m-2 d-flex project p-2">
+            <p key={`p${project.key}`} className="project-des mx-2 p-2">{project.description}</p>
+            <img
+              src={require(`../../assets/images/${project.photo}`)}
+              alt={project.name}
+              key={`img${project.key}`}
+              width="500"
+              height="300"
+            />
+            <a
+              href={project.repo}
+              rel="noopener noreferrer"
+              target="_blank"
+              key={`anchor${project.key}`}
+              className="project-gh text-decoration-none px-2"
+            >
+              Project GitHub Repository
+            </a>
+          </div>
+        </div>
+      ))}
+    </>
         <button type="button" onClick={onClose}>
           X
         </button>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,75 +1,120 @@
 import React, { useState} from "react";
-import Modal from "../Modal";
 
-function Portfolio({ category }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentProject, setCurrentProject] = useState();
+
+function Portfolio() {
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [currentProject, setCurrentProject] = useState();
   
   
   const [projects] = useState([
     {
       name: 'chewview',
-      category: 'chewview',
+      repo: 'https://github.com/ChristopherLawn/team-1-dinner-movie',
+      key: 0,
+      live: "https://christopherlawn.github.io/team-1-dinner-movie/",
       description:
-        "The purpose of this website is to simplify date nights and speed up decision-making! Chew View solves a multitude of problems - particularly when you're on the move, stuck on which movie and food pairing to pick, or simpily trying to figure out some options. For example, a user is going on a date night with their partner. The user would turn to Chew View to easily pick a food option and a random movie that they can get excited for. The application facilitates quick API calls which allow the user to: get restaurant selections based on zip code, store the user's previous zip code searches for faster selection, and provide random movie selections or selections based on the user's keyword or title search!"
+        "The purpose of this website is to simplify date nights and speed up decision-making! Chew View solves a multitude of problems - particularly when you're on the move, stuck on which movie and food pairing to pick, or simpily trying to figure out some options. For example, a user is going on a date night with their partner. The user would turn to Chew View to easily pick a food option and a random movie that they can get excited for. The application facilitates quick API calls which allow the user to: get restaurant selections based on zip code, store the user's previous zip code searches for faster selection, and provide random movie selections or selections based on the user's keyword or title search!",
+        image: "chewview.png"
     },
     {
       name: 'pantry',
-      category: 'pantry',
+      repo: 'https://github.com/zaclark369/Pantry',
+      key: 2,
+      live: "",
       description:
-      "Have you ever wanted to make dinner but can't think of anything. This application will take what you have in your pantry and suggest appropriate recipes using the ingredients you already have. Create an account and you can save your favorites for future viewing. Keyword buttons will help you quickly reach an easy meal to make and visual recipe cards will show you how good home cooking can look."
+      "Have you ever wanted to make dinner but can't think of anything. This application will take what you have in your pantry and suggest appropriate recipes using the ingredients you already have. Create an account and you can save your favorites for future viewing. Keyword buttons will help you quickly reach an easy meal to make and visual recipe cards will show you how good home cooking can look.",
+      image: "pantry.jpg"
     },
     {
       name: 'passwordgenerator',
-      category: 'passwordgenerator',
+      repo: 'https://github.com/zaclark369/Password-Generator',
+      key: 3,
+      live: "",
       description:
-      "A website that offers options to choose between 4 different character sets and a length, and then generates a password from those selected options."
+      "A website that offers options to choose between 4 different character sets and a length, and then generates a password from those selected options.",
+      image: "password.jpg"
     },
     {
       name: 'socialnetwork',
-      category: 'socialnetwork',
+      repo: 'https://github.com/zaclark369/have-you-tried-being-social',
+      key: 4,
+      live: "",
       description:
-      "The backend of a social network app. This backend will allow creation of users, thoughts, reactions, and freinding/unfriending other users."
+      "The backend of a social network app. This backend will allow creation of users, thoughts, reactions, and freinding/unfriending other users.",
+      image: "social.jpg"
     },
     {
       name: 'Jumping Frying Pan',
-      category: 'jumpingfryingpan',
+      repo: 'https://github.com/zaclark369/jumping_fryingpan_sidescroller',
+      key: 5,
+      live: "",
       description:
-        'A CSS based sidescroller game based on the dino game in chrome. It was built as a simple easter egg for the Pantry project'
+        'A CSS based sidescroller game based on the dino game in chrome. It was built as a simple easter egg for the Pantry project',
+        image: "frying.jpg"
     },
     {
       name: 'Employee Tracker',
-      category: 'employeetracker',
+      repo: 'https://github.com/zaclark369/CMS_Employee_Tracker',
+      key: 6,
+      live: "",
       description:
-      "AS A business owner I WANT to be able to view and manage the departments, roles, and employees in my company SO THAT I can organize and plan my business"
+      "AS A business owner I WANT to be able to view and manage the departments, roles, and employees in my company SO THAT I can organize and plan my business",
+      image: "employee.jpg"
     },
   ]);
   
-  const currentProjects = projects.filter(project => project.category === category);
+  // const currentProjects = projects.filter(project => project.repo === repo);
 
-  const toggleModal = (image) => {
-    setCurrentProject({ ...image });
-    setIsModalOpen(!isModalOpen);
-  };
+  // const toggleModal = (image) => {
+  //   setCurrentProject({ ...image });
+  //   setIsModalOpen(!isModalOpen);
+  // };
 
   return (
-    <div>
-      {isModalOpen && (
-        <Modal onClose={toggleModal} currentPhoto={currentProject} />
-      )}
-      <div className="flex-row">
-        {currentProjects.map((image, i) => (
-          <img
-            src={require(`../../assets/projects/${category}/.jpg`).default}
-            alt={image.name}
-            className="img-thumbnail mx-1"
-            onClick={() => toggleModal(image, i)}
-            key={image.name}
-          />
-        ))}
-      </div>
+    // <div>
+    //   {isModalOpen && (
+    //     <Modal onClose={toggleModal} currentProject={currentProject} />
+    //   )}
+    // </div>
+
+<>
+{projects.map((project) => (
+  <div className="py-3" key={`portfolio${project.key}`}>
+    <h2 key={`${project.key}`}>
+      <a
+        href={project.projectDeployment}
+        rel="noopener noreferrer"
+        target="_blank"
+        className="text-decoration-none project-header"
+      >
+        {project.title}
+      </a>
+    </h2>
+    <div key={`div${project.key}`} className="m-2 d-flex project p-2">
+      <p key={`p${project.key}`} className="project-des mx-2 p-2">{project.description}</p>
+      {/* <img
+        src={require(`../../assets/images/${project.image}`)}
+        alt={project.title}
+        key={`img${project.key}`}
+        width="500"
+        height="300"
+      /> */}
+      <a
+        href={project.projectRepo}
+        rel="noopener noreferrer"
+        target="_blank"
+        key={`anchor${project.key}`}
+        className="project-gh text-decoration-none px-2"
+      >
+        Project GitHub Repository
+      </a>
     </div>
-  );
+  </div>
+))}
+</>
+
+);
+  
 };
 
 export default Portfolio;
