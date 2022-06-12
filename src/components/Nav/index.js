1d43980832ddbd7871e98d0d54cbc5fd8e3d77a4
zaclark369 <zacclark369@gmail.com>
2022-06-12 12:10:02 -0700

    ionic import not working as expected. Will just use SVGs

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,56 +1,51 @@
 import React from "react";
+import '@ionic/react/css/core.css';
 
 function Nav(props) {
   const { view, setView } = props;
 
   return (
-    <div className = "navigation">
-          <ul>
-            {/* <li className="nav-link" onClick={() => props.handleView('default')}>Home
+    <div className="navigation">
+      <ul>
+        {/* <li className="nav-link" onClick={() => props.handleView('default')}>Home
                             </li> */}
 
-            <li className="list">
-              <a href="#About" className="btn" onClick={() => setView("about")}>
-                <span className="icon"></span>
-                <span className="text">About</span>
-              </a>
-            </li>
-            <li className="list">
-              <a
-                href="#Portfolio"
-                className="btn"
-                onClick={() => setView("portfolio")}
-              >
-                <span className="icon"></span>
-                <span className="text">Portfolio</span>
-              </a>
-            </li>
-            <li className="list">
-              <a
-                href="#Resume"
-                className="btn"
-                onClick={() => setView("resume")}
-              >
-                <span className="icon"></span>
-                <span className="text">Resume</span>
-              </a>
-            </li>
+        <li className="list">
+          <a href="#About" className="btn" onClick={() => setView("about")}>
+            <span className="icon">
+              <ion-icon name="person-circle-outline"></ion-icon>
+            </span>
+            <span className="text">About</span>
+          </a>
+        </li>
+        <li className="list">
+          <a
+            href="#Portfolio"
+            className="btn"
+            onClick={() => setView("portfolio")}
+          >
+            <span className="icon"></span>
+            <span className="text">Portfolio</span>
+          </a>
+        </li>
+        <li className="list">
+          <a href="#Resume" className="btn" onClick={() => setView("resume")}>
+            <span className="icon"></span>
+            <span className="text">Resume</span>
+          </a>
+        </li>
 
-            <li className="list">
-              <a
-                href="#Contact"
-                className="btn"
-                onClick={() => setView("contact")}
-              >
-                <span className="icon"></span>
-                <span className="text">Contact</span>
-              </a>
-            </li>
-            {/* <li className="nav-link">
+        <li className="list">
+          <a href="#Contact" className="btn" onClick={() => setView("contact")}>
+            <span className="icon"></span>
+            <span className="text">Contact</span>
+          </a>
+        </li>
+        {/* <li className="nav-link">
                 <h5 className='btn'>Full Stack Web Developer</h5>
                 </li> */}
-          </ul>
-        </div>
+      </ul>
+    </div>
   );
 }
 
